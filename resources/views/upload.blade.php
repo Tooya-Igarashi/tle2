b5d21410b8fd77e3658c874c2e70e55f4583f0ed
jfxo0 <janisha.ly.wong@gmail.com>
2025-12-02 12:40:18 +0100

    image upload works

---
+++ resources/views/upload.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<h1> dit is upload page</h1>
+
+
+
+<form action="{{ route('upload.store') }}" method="POST" enctype="multipart/form-data" class="space-y-6 bg-white p-6 rounded-xl shadow">
+    @csrf
+    <div>
+        <label for="content" class="block text-sm font-medium text-gray-700 mb-1">Image</label>
+        <input type="file" name="content" id="content" class="block w-full text-sm text-gray-700 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700  hover:file:bg-indigo-100">
+    </div>
+    <button type="submit" class="inline-flex items-center px-6 py-2.5 rounded-md bg-indigo-600 text-white font-medium hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        Save
+    </button>
+</form>
+
+
+{{--<div class="md:w-1/3">--}}
+{{--    <img src="{{ asset('storage/' . $photos->content) }}"--}}
+{{--         class="rounded-lg w-full object-cover">--}}
+{{--</div>--}}
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,5 +17,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/submit', function () {
+    return view('upload');
+})->name('submit');
+
+Route::resource('upload', UploadController::class);
 
 require __DIR__.'/auth.php';
