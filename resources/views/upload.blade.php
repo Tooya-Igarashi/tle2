7440ec4d70b5d99c5848bfc4a806e808546f523c
jfxo0 <janisha.ly.wong@gmail.com>
2025-12-02 15:14:16 +0100

    sucess notificatie

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -8,6 +8,7 @@
             <label for="content" class="block text-sm font-medium text-gray-700 mb-1">Image</label>
             <input type="file" name="content" id="content" class="block w-full text-sm text-gray-700 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700  hover:file:bg-indigo-100">
         </div>
+
         <button type="submit" class="inline-flex items-center px-6 py-2.5 rounded-md bg-indigo-600 text-white font-medium hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             Save
         </button>
