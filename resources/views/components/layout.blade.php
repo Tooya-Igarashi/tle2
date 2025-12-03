bcd3d12111f45ee11577d1b0bc29568b99c6cfff
jfxo0 <janisha.ly.wong@gmail.com>
2025-12-03 10:18:53 +0100

    nav

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,62 +11,59 @@
     <title>Document</title>
 </head>
 
-<nav>
+<header>
+    <nav class="flex p-4 gap-2 text-base font-medium ">
+        <img src="" alt="logo">
 
+        @if (Route::has('login'))
+            @auth
 
-    @if (Route::has('login'))
-        @auth
-            <li>
                 <a href="{{ route('dashboard') }}"> Home</a>
-            </li>
-            @if(auth()->user()->role)
-                <li>
-{{--                    <a href="{{route('admin.index')}}"--}}
-{{--                       class="flex py-2 text-base font-medium text-body-color hover:text-dark lg:ml-12 lg:inline-flex dark:text-dark-6 dark:hover:text-black">--}}
-{{--                        Admin overview--}}
-{{--                    </a>--}}
-                </li>
+
+                @if(auth()->user()->role)
+                    <li>
+                        {{--                    <a href="{{route('admin.index')}}"--}}
+                        {{--                       class="flex py-2 text-base font-medium text-body-color hover:text-dark lg:ml-12 lg:inline-flex dark:text-dark-6 dark:hover:text-black">--}}
+                        {{--                        Admin overview--}}
+                        {{--                    </a>--}}
+                    </li>
                 @endif
-                </ul>
-</nav>
-</div>
-<a href="">dropdown</a>
-<div class="">
 
-    <form action="{{ route('logout') }}" method="POST">
-        @csrf
-        <button type="submit"
-                class="">
-            Log Out
-        </button>
-    </form>
-</div>
+                <a href="">dropdown</a>
+                <div class="">
+                    <form action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button type="submit" class=""> Log Out </button>
+                    </form>
+                </div>
 
-@else
+            @else
     </nav>
-</div>
+</header>
+
+
 <div class="">
-    <a href="{{route('login')}}"
-       class="">
-        Login
-    </a>
+    <a href="{{route('login')}}" class="">Login </a>
 
     @if (Route::has('register'))
-        <a href="{{route('register')}}"
-           class="">
-            Sign Up
-        </a>
+        <a href="{{route('register')}}" class="">Sign Up </a>
     @endif
 </div>
 @endauth
 @endif
 </nav>
 
-
 <body>
 
 <main>
     {{ $slot }}
 </main>
+
 </body>
+
+<footer class="flex p-4 gap-2 text-base font-medium bg-neutral-400">
+    <a href=""> link</a>
+    <a href=""> link</a>
+    <img src="" alt="logo">
+</footer>
 </html>
