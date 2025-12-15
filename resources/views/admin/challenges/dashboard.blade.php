34e14b67a70903b8c8ff7503166b0ad49be23ec7
tooyaigarashi <igarashitooya5@gmail.com>
2025-12-15 19:36:18 +0100

    made the admin dashboard mobile

--- resources/views/admin/challenges/dashboard.blade.php
+++ resources/views/admin/challenges/dashboard.blade.php
@@ -1,5 +1,6 @@
 <x-app-layout>
-    <table class="mx-auto">
+    <div class="overflow-x-auto shadow-md rounded-lg border border-gray-200">
+        <table class="hidden md:table mx-auto">
         <thead>
         <tr>
             <th class="px-4 py-2">ID</th>
@@ -26,8 +27,8 @@
                         Details &gt;
                     </a>
                 </td>
-                <td class="px-4 py-2">
-                <form method="POST" action="{{ route('admin.authenticate', $challenges->id) }}" class="mt-4">
+                <td class="">
+                <form method="POST" action="{{ route('admin.authenticate', $challenges->id) }}">
                     @csrf
                     @method('PATCH')
                     @if($challenges->published === 0)
@@ -62,4 +63,50 @@
         @endforeach
         </tbody>
     </table>
+    </div>
+    @foreach($challenge as $challenges)
+        <div class="sm:hidden">
+            <div class="bg-white p-4 mb-4 rounded shadow">
+                <div><strong>ID:</strong> {{ $challenges->id }}</div>
+                <div><strong>Name:</strong> {{ $challenges->title }}</div>
+                <div><strong>Description:</strong> {{ Str::limit($challenges->description, 50) }}</div>
+                <div><strong>Created at:</strong>{{$challenges->created_at->format('D-M-Y')}}</div>
+                <div><strong>Updated at:</strong>{{$challenges->updated_at->format('D-M-Y')}}</div>
+
+
+                <div class="mt-2">
+                    <a href="{{ route('challenges.show', $challenges->id) }}" class="inline-block bg-yellow-400 hover:bg-yellow-500 text-black font-semibold px-6 py-3 rounded-lg shadow-lg transition">
+                        Details</a>
+                    <a href="{{ route('admin.edit', $challenges->id) }}"
+                       class="inline-block bg-yellow-400 hover:bg-yellow-500 text-black font-semibold px-6 py-3 rounded-lg shadow-lg transition">
+                        Edit
+                    </a>
+                    <form method="POST" action="{{ route('admin.authenticate', $challenges->id) }}" class="mt-4">
+                        @csrf
+                        @method('PATCH')
+                        @if($challenges->published === 0)
+                            <button type="submit"
+                                    class="bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-md transition-colors">
+                                Authenticate
+                            </button>
+                        @elseif($challenges->published === 1)
+                            <button type="submit"
+                                    class="bg-red-600 hover:bg-red-700 text-white font-medium py-2 px-4 rounded-md transition-colors">
+                                Unauthenticate
+                            </button>
+                        @endif
+                    </form>
+{{--                    <form method="POST" action="{{ route('admin.destroy', $challenges->id) }}" class="mt-4">--}}
+{{--                        @csrf--}}
+{{--                        @method('DELETE')--}}
+{{--                        <button type="submit"--}}
+{{--                                class="bg-red-600 hover:bg-red-700 text-white font-medium py-2 px-4 rounded-md transition-colors">--}}
+{{--                            Delete--}}
+{{--                        </button>--}}
+{{--                    </form>--}}
+                </div>
+
+            </div>
+        </div>
+    @endforeach
 </x-app-layout>
