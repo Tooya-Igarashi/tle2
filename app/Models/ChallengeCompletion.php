6036097980f3790a1c9e821d1b21c01483e83251
jordi <1102000@hr.nl>
2025-12-16 10:16:39 +0100

    migrate en model

---
+++ app/Models/ChallengeCompletion.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ChallengeCompletion extends Model
+{
+    protected $table = 'challenge_completions';
+
+    protected $fillable = [
+        'user_id',
+        'challenge_id',
+        'completed_at',
+    ];
+
+    protected $casts = [
+        'completed_at' => 'datetime',
+    ];
+
+    // Relaties (optioneel maar netjes)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function challenge()
+    {
+        return $this->belongsTo(Challenge::class);
+    }
+}
